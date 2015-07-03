2623bb5cbeb5fc7aa49a3855b2aa20609585fe37
Payton-12301119 <dpcdpc@bjtu.edu.cn>
2015-07-03 21:34:46 +0800

    I don't know what's this...

---
+++ MatchCards/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MatchCards
+//
+//  Created by 布白 on 15/7/3.
+//  Copyright (c) 2015年 DPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
