abda0b88fd1e9e0df462bd1e9657761ef957ee1e
Payton-12301119 <dpcdpc@bjtu.edu.cn>
2015-07-03 21:16:45 +0800

    Single MVC view template.

---
+++ MatchCards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MatchCards
+//
+//  Created by 布白 on 15/7/3.
+//  Copyright (c) 2015年 DPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
