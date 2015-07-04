98c07cc671598d96b7bc56b48e50c95e91db06e3
Payton-12301119 <dpcdpc@bjtu.edu.cn>
2015-07-04 11:05:24 +0800

    Simple version, click card and turn it over.

--- MatchCards/ViewController.m
+++ MatchCards/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 
 @end
 
@@ -22,6 +24,24 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips : %d", self.flipCount];
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"Cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:nil forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"Cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A ♣︎" forState:UIControlStateNormal];
+    }
+    self.flipCount++;
 }
 
 @end
